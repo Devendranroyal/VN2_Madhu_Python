9b4d9180698b064bf4efb8d5118bafdf8ca4ceb8
Devendran D <54701895+Devendranroyal@users.noreply.github.com>
2022-08-12 16:24:32 +0530

    Add files via upload

---
+++ T05_InterviewQuestions.py
@@ -0,0 +1,271 @@
+'''
+MadhuNettem@VN2
+'''
+'''
+I. Introduction:
+=================
+0. Introduce yourself.
+1. Tell me about Python?
+2. Why Python is so popular now a days?
+3. Features of Python
+4. Advantages and Disadvantages of Python 
+5. Interpreted vs Compiled time programming languages. Explain in detail
+6. .py vs .pyc files 
+7. How compilation will happen internally. Explain in detail 
+8. Why Python is Dynamically typed programming Language. Explain 
+9. Python is Platform independent.Explain
+10.Different ways to write python program.
+   Interactive, IDLE, CommandPrompt, IDE 
+   Advantages, Disadvantages
+
+II. Variables:
+===============
+1. x = 10. Explain in detail for CRUD operations
+2. tokens in Python. Explain all types
+3. Garbage collection. How it works internally
+4. Memory Management in Python 
+5. Dynamically typed programming. Explain examples.
+6. Initializing variable, static,dynamic way 
+7. Assigning value to multiple variables. Explain 
+
+III. IDE PyCharm:
+===================
+1. Different IDEs in market 
+2. Advantages of IDE
+3. Shortcuts in PyCharm (Explain min 10)
+
+IV. Operators:
+===============
+1. Explain in detail about all operators 
+2. == vs is 
+3. and or operators. Explain 2 examples 
+4. Operator precedence.
+5. Subtract 2 numbers and print result program.
+	- Write down all scenarios with different input values 
+
+V. DataTypes Intro:
+====================
+1. Importance of DataTypes.2
+2. Different data types, data structures available in Python
+3. int vs float
+4. Boolean. give all scenarios
+5. 0 vs null
+6. Explain each data type, data structure with real life examples.
+7. CRUD Operations.Give examples for each 
+8. Sequences. Types of sequences. Sequence operations
+9. Explain about below functions and give examples 
+	print()
+	id()
+	type()
+	int()
+	float()
+	complex()
+	bool()	
+	input()
+	
+VI. Keyword:
+=====================
+1. Explain all keywords with examples and areas of usage
+
+VII. Decision Making:
+=====================
+1. What is Decision Making. Explain different scenarios when to go for Decision Making
+2. Give examples for below conditions
+	1. single if : 
+	2. if else:
+	3. if elif else:
+	4. if elif elif else:
+	5. Nested if: 
+3. Prepare Programs for below questions
+	1. Prepare state and assign North South West East 
+	    north = []
+		south = ['Andhra Prades', 'Telangana', 'Karnataka','Tamil Nadu', 'Kerala']
+		west = []
+		east = []
+	2. Prepare dictionary with key as state name and value as "list of districts"
+4. Get employee details(in dict format, empid,name,sal, exp) and update hike for employee with below 
+		If exp is 0 to 2 years - 10% Hike
+				  2 to 5 years - 20% Hike
+				  5 to 8 years - 30% Hike
+				  8+           - No hike
+5. Explain below terms in detail 
+	- Requirement
+	- User Criteria
+	- Validations(Client vs Server)
+6. State vs Behavior . Examples
+
+
+VIII. Loops:
+============
+1. Importance of Loops
+2. while loop. Explain in detail with different use cases 
+3. for loop. Explain in detail with different use cases 
+4. while vs for
+5. Give examples while with if else combination 
+6. Give examples for with if else combination
+7. Control statements.Explain and give examples for each keyword 
+    - break 
+    - continue
+    - pass
+8. Implement 5 examples which covers all topics if elif else for while break/continue/pass
+
+Programs:
+----------
+- Between 1 to 100
+    1. Print all numbers  
+    2. Print even numbers
+    3. Print odd numbers 
+    4. Print all prime numbers
+    5. Print numbers with power of 2 (1 2 4 8 16 32 64)
+    6. Print all numbers which are divisible by 5 and 7 
+    7. Print all numbers which are divisible by 4 or 6
+    8. Print first 14 odd numbers 
+    9. Print first 23 even numbers
+   10. Print first 6 numbers which are divisible by 4 and 6 
+   11. Print all numbers except divisible by 9
+   12. Write for loop to explain all data structures.
+   
+IX. Data Structures:
+=====================
+        1. What are CRUD operations.Explain in detail 
+        2. Sequence.Types. Operations on each sequence
+        3. HTTP Request methods for CRUD. Explain in detail 
+     i. Numbers:
+           1. Types of numbers. Explain each use case 
+           2. type conversions
+           3. Explain different operations of boolean type
+    ii. String:
+           1. Explain about string. 
+           2. Multi line string 
+           3. String is Immutable.Explain in detail 
+           4. CRUD Operations on String
+           5. Sequence operations on String
+           6. Memory allocation of String 
+           7. Explain 10 important functions of String
+    iii. List:
+          1. What is use of list. Explain different use cases of List
+          2. Sequence operations on List
+          3. Characteristics(Properties) of List
+          4. CRUD operations on List
+          5. Memory allocation of List
+          6. Write all possible combinations of list structure(homo,hetero with all data types, data structures)
+          7. Explain about each method of List
+              append:
+              pop:
+              ....
+          8. shallow copy vs deep copy 
+          9. append vs extend
+         10. pop vs remove
+         11. Pass by value vs Pass by reference
+    iv. Tuple:
+          1. What is use of Tuple. Explain different use cases of Tuple
+          2. Sequence operations on Tuple
+          3. Characteristics(Properties) of Tuple
+          4. CRUD operations on Tuple
+          5. Memory allocation of Tuple
+          6. Write all possible combinations of Tuple structure(homo,hetero with all data types, data structures)
+          7. Explain about each function of Tuple
+          8. shallow copy vs deep copy in tuple.
+          9. list vs tuple ( Min. 4 differences in detail)
+    v. Dictionary:
+          1. Dictionary realtime usage scenarios
+          2. Properties of Dictionary. Explain in detail 
+          3. Dictionary methods 
+          4. Dictionary is mutable or immutable.Why 
+          5. Explain in dictionary function
+          6. Hashing algorithm in dictionary 
+    vi. Set:
+          1. Properties of Set
+          2. Functions of Set
+          
+X. Functions:
+=============
+    1. What is a function
+    2. Why we need to write functions
+    3. Define function and explain in detail 
+    4. State vs Behavior
+    5. Function types
+    6. Function calling ways 
+    7. How to print function name. Explain how a function will be loaded
+    8. Parameter vs Argument
+    9. Variable vs Value 
+   10. Different ways of calling function 
+   11. Function overloading
+   12. Ananymous function. Explain in detail 
+   13. Lambda with map filter and reduce functions. Explain in detail with examples 
+   14. Function memory allocation
+   15. Scope of variable. Explain about LEGB rule
+
+XI. Packages, Modules:
+=======================
+    1. Importance of Packge vs Module
+    2. file vs module
+    3. package vs module
+    4. Builtin libraries. Explain about min 10 libraries in python 
+    5. from import  vs import (Directly importing module vs importing specific function/class from module)
+    6. Create 2 packages,modules inside it. Call function from first package inside second package module
+    7. __init__.py file in each package. Why. Importance 
+   10. if __name__ == '__main__': What it means ?
+   
+XII. OOPs:
+==============
+    1. What is OOPs
+    2. Why OOPs required
+    3. Features of OOPs
+    4. class vs object 
+    5. class vs instance vs local variables. Explain in detail about usecase of each variable 
+    6. class vs instance vs static method. Explain in detail about usecase of each Method 
+    7. Importance of init method  
+    8. Importance of self
+    9. object life cycle
+   10. Constructor. Explain in detail 
+   11. Constructor life cycle
+   12. Default vs Parameterized Constructor
+   13. Constructor Overloading 
+   14. new vs init
+   15. Explain below concepts in detail with examples
+        a. Encapsulation
+        b. Abstraction
+        c. Inheritance
+        d. Polymorphism
+   16. Method overloading vs Method overriding
+   17. Inheritance types
+   18. MRO principle.Explain in detail 
+   19. Access modifiers in Python 
+   20. Multiple Inheritance. Explain in detail 
+   21. super keyword importance
+   22. Calling super class method from sub class
+   23. Difference between public protected private methods Ex: x() _x() __x()
+   24. Importance of dunder functions
+   25. __str__ vs __repr__
+   26. Abstraction, Abstract class. When to use. Explain in detail 
+   27. Abstract class vs Interface
+
+XIII. Debugging:
+================
+    1. Importance of debugging
+    2. Debugging shortcuts(F6,F5...)
+    3. pdb module
+    4. pdb module commands
+    
+IVX. Exception Handling:
+========================
+    1. Importance of Exception handling
+    2. error vs exception
+    3. Different exception classes used in your project
+    4. try vs except vs else vs finally. Explain in detail 
+    5. Exception order while defining in except blocks
+    6. Multiple ways of writing except blocks
+    7. How to create and raise custom exception 
+
+VX. File Handling:
+====================
+    1. Importance of file
+    2. Different files in general
+    3. File open modes
+    4. How to work with files. Open read/write/append Close. Give examples 
+    5. Importance of context manager.Explain in detail 
+    6. enter vs exit 
+    7. Who to open text,csv,excel,pdf,image,audio,video,jpeg using open and with statements
+    
+'''
